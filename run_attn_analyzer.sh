394e97abfcde694f3e80c59e1a67331a643b65f8
e-schuh <eliassebastian.schuhmacher@uzh.ch>
2025-08-22 21:15:07 +0200

    add attention analyzer

---
+++ run_attn_analyzer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Wiki Parallel Processing Pipeline
+# This script runs all the steps to process Wikipedia parallel data
+
+set -e  # Exit on any error
+
+echo "Starting..."
+
+echo "1; only first token"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token
+
+echo "1; all tokens"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128
+
+echo "2; only first token"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token
+
+echo "2; all tokens"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128
+
+echo "3; only first token"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token
+
+echo "3; all tokens"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128
+
+echo "4; only first token"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token
+
+echo "4; all tokens"
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128
