68289d15ed04cb9fad8cbbf9633959b187e05f04
e-schuh <eliassebastian.schuhmacher@uzh.ch>
2025-08-22 21:39:27 +0200

    change batch size

--- run_attn_analyzer.sh
+++ run_attn_analyzer.sh
@@ -8,25 +8,25 @@ set -e  # Exit on any error
 echo "Starting..."
 
 echo "1; only first token"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 4
 
 echo "1; all tokens"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128 --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_1_en.json --analysis_mode baskets --basket_size 128 --batch_size 4
 
 echo "2; only first token"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 2
 
 echo "2; all tokens"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128 --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_2_en.json --analysis_mode baskets --basket_size 128 --batch_size 2
 
 echo "3; only first token"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 2
 
 echo "3; all tokens"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128 --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_3_en.json --analysis_mode baskets --basket_size 128 --batch_size 2
 
 echo "4; only first token"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128 --only_from_first_token --batch_size 2
 
 echo "4; all tokens"
-poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128 --batch_size 8
+poetry run python -m locobench.attention.attention_analyzer --config config/Alibaba_mGTE/wiki_parallel/embedding_config_wiki_parallel_4_en.json --analysis_mode baskets --basket_size 128 --batch_size 2
